293b6f87a86f75020780d4fc3f5e5db8f48c364d
way2muchnoise <jens.baetens@live.be>
2016-08-30 15:05:18 +0200

    fix template for landing page

--- CurseForgeBadge.py
+++ CurseForgeBadge.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from CFReader import get_downloads
 
 app = Flask(__name__)
@@ -6,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def landing():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 
 @app.route('/<project>.svg')
