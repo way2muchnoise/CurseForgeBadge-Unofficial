2a946a6dd1b3e92686827f504d42b9d59f323ab6
way2muchnoise <jens.baetens@live.be>
2016-08-30 13:52:26 +0200

    allow for all categorys and not just mods
    but default to mods

--- CFReader.py
+++ CFReader.py
@@ -2,8 +2,8 @@ import re
 import urllib2
 
 
-def get_downloads(project_id):
-    response = urllib2.urlopen("https://minecraft.curseforge.com/projects/" + repr(project_id)).read()
+def get_downloads(category, project):
+    response = urllib2.urlopen("https://minecraft.curseforge.com/" + category + "/" + project).read()
     pattern = 'Total Downloads</div>\s+<div class="info-data">(.*?)</div>'
     m = re.search(pattern, response)
     if m:
--- CurseForgeBadge.py
+++ CurseForgeBadge.py
@@ -4,20 +4,29 @@ from CFReader import get_downloads
 app = Flask(__name__)
 
 
-@app.route('/<int:project_id>.svg')
-@app.route('/<style>-<int:project_id>.svg')
-@app.route('/<style>-<int:project_id>-<extra>.svg')
-def hello_world(project_id, style='full', extra=None):
+@app.route('/<project>.svg')
+@app.route('/<style>_<project>.svg')
+@app.route('/<style>_<project>_<extra>.svg')
+@app.route('/<category>/<project>.svg')
+@app.route('/<category>/<style>_<project>.svg')
+@app.route('/<category>/<style>_<project>_<extra>.svg')
+def hello_world(project, style='full', extra=None, category='projects'):
     template = app.open_resource('templates/shield.svg', 'r').read()
-    dls = get_downloads(project_id)
+    replacement = ''
+    dls = get_downloads(category, project)
     if style == 'short':
         splitted = dls.split(',')
-        dls = splitted[0] + ('M+' if len(splitted) > 2 else ('k+' if len(splitted) > 1 else ''))
+        first_numbner = splitted[0][0]
+        padding_zeros = '0' * (len(splitted[0])-1)
+        post_fix = ('M+' if len(splitted) > 2 else ('k+' if len(splitted) > 1 else ''))
+        replacement += first_numbner + padding_zeros + post_fix
+    else:
+        replacement += dls
     if extra:
-        dls += " " + extra
-    width = max(len(dls) * 7 + 12, 40)
+        replacement += " " + extra
+    width = max(len(replacement) * 7 + 12, 40)
     return template\
-        .replace('{{dls}}', dls)\
+        .replace('{{dls}}', replacement)\
         .replace('{{width}}', repr(width))\
         .replace('{{totalWidth}}', repr(30 + width))\
         .replace('{{offset}}', repr(30.5 + width / 2)), 200, {'Content-Type': 'image/svg+xml'}
