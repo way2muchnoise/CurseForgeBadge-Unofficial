61d91f5f92b8a1116f71a274a127ae1cbf6b00a3
way2muchnoise <jens.baetens@live.be>
2016-08-31 17:16:20 +0200

    str or unicode what evs

--- badgeCreator.py
+++ badgeCreator.py
@@ -1,4 +1,4 @@
 def create_badge(template, **kwargs):
     for key, value in kwargs.iteritems():
-        template = template.replace('{{' + key + '}}', value if isinstance(value, str) else repr(value))
+        template = template.replace('{{' + key + '}}', value if isinstance(value, str) or isinstance(value, unicode) else repr(value))
     return template
