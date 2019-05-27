463272eaf19931ccdf5ecba995cba4e2b372f88f
Jared <jaredlll08@gmail.com>
2019-05-27 15:59:35 +0200

    added /title endpoint

--- CurseForgeBadge.py
+++ CurseForgeBadge.py
@@ -13,6 +13,20 @@ def landing():
     return render_template('index.html')
 
 
+@app.route('/title/<project>.svg')
+@app.route('/title/<project>(<l_colour>).svg')
+@app.route('/title/<project>(<l_colour>-<r_colour>-<text_colour>-<shadow_colour>-<logo_colour>).svg')
+def title(project, l_colour='E04E14', r_colour='2D2D2D', text_colour='fff',
+          shadow_colour='010101', logo_colour='1C1C1C'):
+    template = open_template('curseShield.svg', request.args)
+    replacement = CFReader.get_title(project)
+
+    width = max(len(replacement) * 7 + 12, 40)
+    return create_badge(template, dls=replacement, width=width, totalWidth=(30 + width),
+                        offset=(30.5 + width / 2), l_colour=l_colour, r_colour=r_colour, text_colour=text_colour,
+                        shadow_colour=shadow_colour, logo_colour=logo_colour), 200, {'Content-Type': 'image/svg+xml'}
+
+
 @app.route('/<project>.svg')
 @app.route('/<project>(<l_colour>).svg')
 @app.route('/<style>_<project>.svg')
@@ -75,7 +89,7 @@ def supported_versions(project, style='all', text='Available for', l_colour='2D2
 @app.route('/packs/<style>_<project>_<before>_<after>(<l_colour>).svg')
 @app.route('/packs/<style>_<project>_<before>_<after>(<l_colour>-<r_colour>-<text_colour>-<shadow_colour>-<logo_colour>).svg')
 def packs(project, style='full', before='included in', after='packs', l_colour='E04E14', r_colour='2D2D2D', text_colour='fff',
-              shadow_colour='010101', logo_colour='1C1C1C'):
+          shadow_colour='010101', logo_colour='1C1C1C'):
     template = open_template('curseShield.svg', request.args)
     replacement = before + ' '
     packs = CFReader.get_dependents(project, 'included')
@@ -103,7 +117,7 @@ def packs(project, style='full', before='included in', after='packs', l_colour='
 @app.route('/mods/<style>_<project>_<before>_<after>(<l_colour>).svg')
 @app.route('/mods/<style>_<project>_<before>_<after>(<l_colour>-<r_colour>-<text_colour>-<shadow_colour>-<logo_colour>).svg')
 def mods(project, style='full', before='required for', after='mods', l_colour='E04E14', r_colour='2D2D2D', text_colour='fff',
-              shadow_colour='010101', logo_colour='1C1C1C'):
+         shadow_colour='010101', logo_colour='1C1C1C'):
     template = open_template('curseShield.svg', request.args)
     replacement = before + ' '
     packs = CFReader.get_dependents(project, 'required')
