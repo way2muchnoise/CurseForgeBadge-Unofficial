cf8ab783ab190cc9524a7ad7375104262529c5f7
way2muchnoise <jens.baetens@live.be>
2019-06-27 19:26:57 +0200

    fixes for new curse web pages

--- CFReader.py
+++ CFReader.py
@@ -4,15 +4,22 @@ from lxml import html
 from lxml.cssselect import CSSSelector
 
 
-def get_project(project):
+def get_project(project, re_add_part=''):
     opener = urllib2.build_opener()
     opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
     opened = opener.open("https://minecraft.curseforge.com/projects/" + project)
-    if '?' in project:
+    if '?' in project or re_add_part != "":
         opened_url = opened.geturl()
-        params = project.split('?')[-1]
-        if params not in opened_url:
-            opened = opener.open(opened_url + '?' + params)
+        if '?' in project:
+            params = project.split('?')[-1]
+            if params not in opened_url:
+                opened_url += '?' + params
+        if re_add_part != "":
+            url, url_params = opened_url.split('?')
+            parts = url.split('/')[-re_add_part.count('/'):]
+            if parts != re_add_part.split('/'):
+                opened_url = url + re_add_part + '?' + url_params
+        opened = opener.open(opened_url)
     return opened.read()
 
 
@@ -22,7 +29,7 @@ def get_files(project):
 
 def get_downloads(project):
     response = get_project(project)
-    pattern = r'Total Downloads\s*</div>\s*<div class="info-data">(.*?)</div>'
+    pattern = r'Total Downloads</span>\s*<span>(.*?)</span>'
     m = re.search(pattern, response)
     if m:
         return m.group(1)
@@ -44,9 +51,20 @@ def get_versions(project):
         return ['Error']
 
 
+# Currently only display latest version with Release version
+def get_version(project):
+    response = get_project(project)
+    pattern = r'Game Version: (.*?)</span>'
+    m = re.search(pattern, response)
+    if m:
+        return m.group(1)
+    else:
+        return 'Error'
+
+
 def get_title(project):
     response = get_project(project)
-    pattern = r'<h1 class="project-title.*?">\s+<a.*?>\s+<span class="overflow-tip">(.*?)\s*</span></a>\s+</h1>'
+    pattern = r'<h2 class="font-bold text-lg break-all">(.*?)</h2>'
     m = re.search(pattern, response)
     if m:
         return m.group(1)
@@ -65,14 +83,14 @@ dependents_dict = {
 
 
 def get_dependents(project, type):
-    response = get_project(project + '/relations/dependents?filter-related-dependents=' + dependents_dict[type])
-    pagination_pattern = r'<a href="(.*?)" class="b-pagination-item">(\d+)</a>'
+    response = get_project(project + '/relations/dependents?filter-related-dependents=' + dependents_dict[type], '/relations/dependents')
+    pagination_pattern = r'<a href="(.*?)" class="pagination-item"><span class="text-primary-500">(\d+)</span></a>'
     pages = re.findall(pagination_pattern, response)
     current_page = 1
     if len(pages) > 0:
         largest = sorted(pages, key=lambda page: int(page[1]))[-1]
-        response = get_project(largest[0][len('/projects/'):])
+        response = get_project(largest[0][len('/minecraft/mc-mods/'):], '/relations/dependents')
         current_page = int(largest[1])
-    pattern = r'<li class="project-list-item">'
+    pattern = r'<li class="project-listing-row'
     items = re.findall(pattern, response)
     return "{:,}".format(len(items) + (current_page - 1) * 20)
