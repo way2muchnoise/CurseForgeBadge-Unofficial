b5a7425ee65061a9da0dd21b4f0bdb9d8df8f5d6
way2muchnoise <jens.baetens@live.be>
2022-01-20 17:45:03 +0100

    fix supported versions

--- CFReader.py
+++ CFReader.py
@@ -61,7 +61,7 @@ def get_versions(project):
     if api_response:
         results = [  # Only take major versions
             re.sub(r'(\d+)\.(\d+)(\.\d+)?', r'\1.\2', gameVersionLatestFile['gameVersion'])
-            for gameVersionLatestFile in api_response['gameVersionLatestFiles']
+            for gameVersionLatestFile in api_response['latestFilesIndexes']
         ]
         return list(sorted(set(results), reverse=True, key=lambda s: list(map(int, s.split('.')))))
     else:
